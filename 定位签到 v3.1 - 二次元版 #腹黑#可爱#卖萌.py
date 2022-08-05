9b45e3326e0104122e47a3cfdb2084c8c13d3846
Dominic-KK <Dominic548@foxmail.com>
2022-08-05 16:47:10 +0800

    upload v3.1

--- 定位签到 v3.1 - 二次元版 #腹黑#可爱#卖萌.py
+++ 定位签到 v3.1 - 二次元版 #腹黑#可爱#卖萌.py
@@ -5,17 +5,14 @@ import time
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
-# **********************************************************************************************************
-#                 定位签到强制签到版
-#               请注意，本版本使用请慎重
-# 本版本可以突破打卡时间限制，例如未到打卡时间，或超过打卡时间都可以打卡，甚至你还可以覆盖打卡（慎重使用）
-#                   只要你不想被发现
-# 推荐适用范围：
-# 1.忘记打卡可以用这个补卡（挂了云函数后理论上不存在）
-# 2.签到扣打卡的补卡（可以绕过物理设备的限制）
-#                 综上所述，本版本的使用请慎重
+# ***************************************************************
+#                 定位签到V3.1
+#                   更新日志
+# 1.修复重复打卡的BUG。
+# 2.修复超时后还能打卡的BUG（超时想补卡会单独出一个脚本）
+# 3.统一了一下语气，改成了腹黑可爱会卖萌的妹子
 #               @author：Smallway
-# **********************************************************************************************************
+# ***************************************************************
 
 
 def handler():
@@ -80,47 +77,51 @@ def handler():
     getdata = getdata['data']  # 获取返回值中的data数据
     getdata = getdata[0]
     print(getdata)
-    print("本公主正在为你打卡，好好给本公主等着")
-    realdata = {
-        "id": str(getdata['logId']),
-        "signId": str(getdata['id']),
-        "latitude": latitude,
-        "longitude": longitude,
-        "country": country,
-        "province": province,
-        "city": city,
-        "district": district,
-        "township": township,
-    }
-    print(realdata)
-    api = "http://student.wozaixiaoyuan.com/sign/doSign.json/"
-    signheaders = {
-        "Host": "student.wozaixiaoyuan.com",
-        "Content-Type": "application/json",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "User-Agent": str(User_Agent),
-        "Referer": str(Referer),
-        "Content-Length": "500",
-        "Cookie": "",
-        "JWSESSION": str(jwsession),
-        "charset": "utf-8",
-    }
-    res = requests.post(api, headers=signheaders, json=realdata)  # 打卡提交
-    time.sleep(1)
-    res = eval(res.text)
-    if res['code'] == 0:
-        message = "本公主给你打过卡了，还不快跪下？"
-        state = "打卡成功"
-        mail(message, state, sender, pass_, user)
-        print("打卡成功"+str(res))
-        return "打卡成功"+str(res)
-    else:
-        message = "人...人家也不知道为什么会出错，喏，这是错误信息，请大人过目："+str(res)
-        state = "打卡失败"
-        mail(message, state, sender, pass_, user)
-        print("打卡失败"+str(res))
-        return "打卡失败"+str(res)
+    if getdata['type'] == 1:
+        print("你已经签过道了，不要为难本公主好嘛")
+        return "你已经签过道了，不要为难本公主好嘛"
+    elif getdata['type'] == 0:
+        print("本公主正在为你打卡，好好给本公主等着")
+        realdata = {
+            "id": str(getdata['logId']),
+            "signId": str(getdata['id']),
+            "latitude": latitude,
+            "longitude": longitude,
+            "country": country,
+            "province": province,
+            "city": city,
+            "district": district,
+            "township": township,
+        }
+        print(realdata)
+        api = "http://student.wozaixiaoyuan.com/sign/doSign.json/"
+        signheaders = {
+            "Host": "student.wozaixiaoyuan.com",
+            "Content-Type": "application/json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "User-Agent": str(User_Agent),
+            "Referer": str(Referer),
+            "Content-Length": "500",
+            "Cookie": "",
+            "JWSESSION": str(jwsession),
+            "charset": "utf-8",
+        }
+        res = requests.post(api, headers=signheaders, json=realdata)  # 打卡提交
+        time.sleep(1)
+        res = eval(res.text)
+        if res['code'] == 0:
+            message = "本公主给你打过卡了，还不快跪下？"
+            state = "打卡成功"
+            mail(message, state, sender, pass_, user)
+            print("打卡成功"+str(res))
+            return "打卡成功"+str(res)
+        else:
+            message = "人...人家也不知道为什么会出错，喏，这是错误信息，请大人过目："+str(res)
+            state = "打卡失败"
+            mail(message, state, sender, pass_, user)
+            print("打卡失败"+str(res))
+            return "打卡失败"+str(res)
 
 
 def mail(message, state, sender, pass_, user):
